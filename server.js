2b70dfa3939a6eec95eaf39ed7ec6710da0df449
Navneet Vaid <navneet.vaid5@gmail.com>
2020-11-27 18:12:27 +0530

    Added Express server for commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static('./dist/spacex'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/spacex/'}
+  );
+  });
+  app.listen(process.env.PORT || 8080);
